f6e4c45c03bd418b422de54eb80d1eb4d694cd38
Hsin tung <hsintung@Hsins-MacBook-Pro.local>
2020-02-06 09:54:51 -0800

    Implement proxy server

---
+++ ProxyServer/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+const path = require('path');
+const axios = require('axios');
+
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use(express.json());
+
+app.get('/houses', (req, res) => {
+  axios.get('http://localhost:3004/houses')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+app.get('/seed', (req, res) => {
+  axios.get('http://localhost:3003/seed')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+app.get('/listings', (req, res) => {
+  axios.get('http://localhost:3001/listings')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+app.get('/api/summary/data/:id', (req, res) => {
+  axios.get('http://localhost:3002/api/summary/data/:id')
+  .then((response) => {
+    res.send(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
